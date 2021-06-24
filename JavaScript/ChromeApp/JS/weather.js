34caba9a87ef2f9046ad175e55a8f1bc6a29cc71
Ted <hayejun1013@naver.com>
2021-06-25 01:57:34 +0900

    HTML, JS : Add Weather API

---
+++ JavaScript/ChromeApp/JS/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "b4e60252303940c926699e3d50edfb92";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = data.weather[0].main;
+      city.innerText = `${data.name} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
